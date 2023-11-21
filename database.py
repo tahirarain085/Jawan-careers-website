de60275b6392875fcfdf0833da65efad8a772cb8
tahirarin085 <tahirarin085@gmail.com>
2023-11-21 13:36:22 +0500

    database modified

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine,text
 
 
-connection_string = "mysql+pymysql://e78mu9mp4geh687dh8s5:pscale_pw_GAXHDAq5ALPyzh8kZFIbgcCSI3sAguMvakwUPvyoZkw@aws.connect.psdb.cloud:3306/jawancareers"
+connection_string = "mysql+pymysql://fujs4gbqp1m703t2qo78:pscale_pw_VLAKFeA5gNmVQw6YGlbmWidYG7P4AmRnzgkaZctvmnk@aws.connect.psdb.cloud:3306/jawancareers"
 engine = create_engine(connection_string, connect_args = {
     "ssl":{
         "ssl_ca": "/etc/ssl/cert.pem"
@@ -9,11 +9,19 @@ engine = create_engine(connection_string, connect_args = {
 })
 def load_jobs_from_db():
     with engine.connect() as conn:
-        results = conn.execute(text("SELECT * FROM Jobs"))
-
-        jobs = {}
-        for row in results.all():
-            jobs.update(row)
-            
+        result = conn.execute(text('SELECT * FROM jobs'))
+        rows = result.fetchall()
+        jobs = [dict(zip(result.keys(), row)) for row in rows]
         return jobs
+
+
+# def load_jobs_from_db():
+#     with engine.connect() as conn:
+#         results = conn.execute(text("SELECT * FROM jobs"))
+
+#         jobs = {}
+#         for row in results.all():
+#             jobs.update(row)
+            
+#         return jobs
      
